985051ca81990fc61304cefc29e7f7cc8033b7f3
Kevin <ksk382@nyu.edu>
2020-08-18 15:08:49 -0400

    Fixing settings.py

--- regletters/settings.py
+++ regletters/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2u-5$lvj7-^s2#a((n^jhw01l!p+bs1ky!q_-ig^(s6n2lhi80'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
